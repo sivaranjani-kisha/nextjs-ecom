c98beb11f62a46723fe32c9cf11f3eae6f5f093e
AjithMDev <ajithajith33398@gmail.com>
2025-09-12 16:59:38 +0530

    button border added

--- components/AddToCart.jsx
+++ components/AddToCart.jsx
@@ -144,7 +144,7 @@ if (selectedFrequentProducts?.length > 0) {
       <button 
   onClick={handleAddToCart}
   disabled={isLoading || isOutOfStock || isprice}
-  className={`w-full sm:w-auto px-2 py-2 md:px-3 md:py-2 rounded-md shadow-md  transition duration-300 text-sm flex items-center justify-center gap-x-1 
+  className={`w-full sm:w-auto px-2 py-2 md:px-3 md:py-2 border border-1 rounded-md  transition duration-300 text-sm flex items-center justify-center gap-x-1 
     ${isOutOfStock
           ? 'bg-gray-400 cursor-not-allowed text-white'
           :
