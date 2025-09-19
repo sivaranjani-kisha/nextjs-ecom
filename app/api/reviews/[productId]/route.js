e34bc848d7ebab00d933c5c333c1d216e5495351
sivaranjani-kisha <msivaranjani2036@gmail.com>
2025-09-19 11:22:56 +0530

    review changes

--- app/api/reviews/[productId]/route.js
+++ app/api/reviews/[productId]/route.js
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/db";
 import Review from "@/models/Review";
+import User from "@/models/User";
 import mongoose from "mongoose";
 
 export async function GET(request, { params }) {
