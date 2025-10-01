9c8f301d31d7e2d05cb4f8abc8341ed51d44de11
Sivaranjani <msivaranjani2036@gmail.com>
2025-10-01 13:34:27 +0530

    checkout

--- components/checkout/checkout.js
+++ components/checkout/checkout.js
@@ -796,7 +796,7 @@ const grandTotal = subtotal - totalDiscount;
           JSON.stringify([name,addressData.email,addressData.phonenumber,deliveryAddress, adminItemsTableHtml])
         );
 
-        const emailadmin = ["arunkarthik@bharathelectronics.in","ecom@bharathelectronics.in","itadmin@bharathelectronics.in","telemarketing@bharathelectronics.in","sekarcorp@bharathelectronics.in"];
+        const emailadmin = ["arunkarthik@bharathelectronics.in","ecom@bharathelectronics.in","itadmin@bharathelectronics.in","telemarketing@bharathelectronics.in","sekarcorp@bharathelectronics.in","siva96852@gmail.com"];
 
         // const emailadmin = ["siva96852@gmail.com"];
         emailadmin.forEach(async (adminEmail) => {
@@ -812,7 +812,7 @@ const grandTotal = subtotal - totalDiscount;
         let adminData = await adminresponse.json();
         });
 
-        
+
         toast.success("Order placed successfully!");
         router.push('/orders');
       }
